7090cf2ec888b9cce754dfc0c460018b454bcd21
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-11-23 20:31:23 +0700

    feat: Nilable::Streams::MapMulti

--- functionals/src/test/java/NilableTest.java
+++ functionals/src/test/java/NilableTest.java
@@ -3,8 +3,15 @@ import org.junit.jupiter.api.Test;
 
 final class NilableTest {
     @Test
-    void staticFactory_Of() {
+    void static_Factory_Of() {
         Assertions.assertTrue(Nilable.of(null) instanceof Nilable.Empty, "Instantiating empty Nilable");
         Assertions.assertTrue(Nilable.of(new Object()) instanceof Nilable.Has, "Instantiating container Nilable.Has");
+    }
+
+    @Test
+    void super_Object_Equals() {
+        Assertions.assertFalse(Nilable.empty().equals(Nilable.of(new Object())));
+        record Sample(int n) {}
+        Assertions.assertTrue(Nilable.of(new Sample(10)).equals(Nilable.of(new Sample(10))));
     }
 }
