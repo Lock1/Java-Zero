55d7abaf0475430281ae0979dce8797e05a3ea35
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-11-25 02:02:09 +0700

    test: Some Nilable::to() & More Javadoc

--- functionals/src/main/java/Nilable.java
+++ functionals/src/main/java/Nilable.java
@@ -198,17 +198,34 @@ public sealed interface Nilable<T> { // Post-Valhalla: public sealed abstract va
       *
       * This method has a neat (and bit quirky) interaction with static functions, due to how Java method reference &amp; parametrized type works.<br/>
       * For example, {@code nilableFaulty.to(Faulty::ofTransposed)}.<br/>
-      * It's somewhat possible to do compile-time type checking on parametrized type, but this mechanic has limitation with subtyping.<br/>
-      * Function interface might need to be explicitly declared as covariant in order to work with this method.<br/>
-      * Example: {@code Function<Nilable<Exception>,String> f; Nilable.of(new RuntimeException()).to(f);}<br/><br/>
       *
+      * <h4>Dealing with subtyping</h4>
+      * With static functions, it's possible to emulate other languages compile-time type checking on parametrized types like what happen here.<br/> 
+      * However, this mechanic has some limitation when used alongside subtyping, so additional care is required when defining function accepting parametrized type like {@link Nilable}.<br/><br/>
+      *
+      * Functional interfaces need to be explicitly declared as covariant in order to work with this method.<br/>
+      * Example: <pre>{@code
+      * Function<Nilable<? extends Exception>,String> f = ex -> { ... }; // Covariance: ? extends Exception
+      * Nilable.of(new RuntimeException())                               // RuntimeException is subtype of / extends Exception
+      *     .to(f);
+      * }</pre>
+      * For static functions, declare any function working with {@link Nilable} to be generic static function with appropriate type constraint / bound.<br/>
+      * Example <pre>{@code
+      * static <T extends Exception> String f(Nilable<T> ex) { ... } // Similar to functional interfaces, declare type `T` in f(Nilable<T>) to be covariant
+      * void main() {
+      *     Nilable.of(new RuntimeException())                       // Exception :> RuntimeException
+      *         .to(Main::f);
+      * }
+      * }</pre>
+      *
+      * <h4>Flexibility &amp; extension functions</h4>
       * It's not restricted to pre-made function inside this library, it's up to user code to define convenience function that suits their needs.<br/>
       * It also can be thought like Kotlin's extension functions, but without terrible namespace pollution it brings (opt-in explicit invocation of {@link #to(Function)}).
       *
       * @param <R> Any type
       * @param transmutator {@link Nilable} type transformer. Use explicit type witnesses if needed, ex: {@code Function<? super Nilable<? extends T>,? extends R>}
       * @return Transmutation result */
-    public default <R> R to(Function<? super Nilable<T>,? extends R> transmutator) { // Deliberately made type parameter T to be invariant, let the user code define it
+    public default <R> R to(Function<? super Nilable<T>,? extends R> transmutator) { // Deliberately made type parameter T to be invariant
         return transmutator.apply(this);
     }
 
--- functionals/src/test/java/NilableTest.java
+++ functionals/src/test/java/NilableTest.java
@@ -1,3 +1,5 @@
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,21 @@ final class NilableTest {
         Assertions.assertFalse(Nilable.empty().equals(Nilable.of(new Object())));
         record Sample(int n) {}
         Assertions.assertTrue(Nilable.of(new Sample(10)).equals(Nilable.of(new Sample(10))));
+    }
+
+    @Test
+    void method_To() {
+        final Faulty<Nilable<String>,Integer> transposedNilable = Nilable.of(Faulty.<String,Integer>ofError(100))
+            .to(Faulty::ofTransposed);
+        Assertions.assertTrue(transposedNilable.equals(Faulty.ofError(100)), "Nilable::to(Faulty::ofTransposed()) nice behavior");
+
+        final Function<Nilable<? extends Exception>,String> functionalInterface = ex -> ex.map(Exception::getMessage).orElse("10");
+        final String mapOrElseResult = Nilable.of(new RuntimeException())
+            .to(functionalInterface);
+        Assertions.assertTrue(mapOrElseResult.equals(Nilable.of(new Exception()).to(NilableTest::fun)), "Functional interface vs static function produces equals()");
+    }
+
+    private static <T extends Exception> String fun(Nilable<T> e) {
+        return e.map(Exception::getMessage).orElse("10");
     }
 }
