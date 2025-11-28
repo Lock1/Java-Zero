75c21c75a83742ae9321eac01945022a518daca0
Lock1 <30568743+Lock1@users.noreply.github.com>
2025-11-28 19:20:45 +0700

    feat: FunctionalDatas.TupleOf2 Map.Entry

--- functionals/src/main/java/FunctionalDatas.java
+++ functionals/src/main/java/FunctionalDatas.java
@@ -1,9 +1,14 @@
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /** Namespace for functional programming common data structures. */
 public enum FunctionalDatas { ;
-    public record TupleOf2<T1,T2>(T1 t1, T2 t2) {}
+    public record TupleOf2<T1,T2>(T1 t1, T2 t2) implements Map.Entry<T1,T2> {
+        @Override public T1 getKey() { return this.t1; }
+        @Override public T2 getValue() { return this.t2; }
+        @Override public T2 setValue(T2 arg0) { throw new BuggyCodeException("TupleOf2 does not support Map.setValue()"); }
+    }
     public record TupleOf3<T1,T2,T3>(T1 t1, T2 t2, T3 t3) {}
     public record TupleOf4<T1,T2,T3,T4>(T1 t1, T2 t2, T3 t3, T4 t4) {}
     public record TupleOf5<T1,T2,T3,T4,T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) {}
